b6c2027eed341c97be18bf41c24b8121c65f6479
yeswanth <yswnth.2010@gmail.com>
2024-09-01 18:25:32 +0530

    shell practice

--- 19-top-5-repeated-words.sh
+++ 19-top-5-repeated-words.sh
@@ -2,7 +2,7 @@
 
 
 FILE_NAME=$1
-
+WORD_COUNT=${2:-5}
 
 FILE=$(find $FILE_NAME)
 
@@ -10,7 +10,7 @@ FILE=$(find $FILE_NAME)
 if [ ! -z $FILE ]
 then
     echo "$FILE_NAME exists"
-    Freq_Five_Words=$(tr '[:space:]' '[\n*]' < ${FILE_NAME} | sort | uniq -c | sort -nr | head -5)
+    Freq_Five_Words=$(tr '[:space:]' '[\n*]' < ${FILE_NAME} | tr 'A-Z' 'a-z' | sort | uniq -c | sort -nr | head -$WORD_COUNT)
     echo "$Freq_Five_Words"
 else
     echo "$FILE_NAME does not exist"
