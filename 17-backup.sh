f5d4964ef03f124dbdfb8dc747de26a61cfde99c
yeswanth <yswnth.2010@gmail.com>
2024-09-01 17:11:57 +0530

    shell practice

--- 17-backup.sh
+++ 17-backup.sh
@@ -49,7 +49,7 @@ else
     #check if zip file is successfully created or not
     if [ -f $ZIP_FILE ]
     then
-        echo -e "$B Successfully zippped $N files older than $DAYS days"
+        echo -e "$G Successfully zippped $N files older than $DAYS days"
         #remove the files after zipping
         while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
         do
