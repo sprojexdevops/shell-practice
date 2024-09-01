d05b0c78ccc505a27ac1ce5721635f56379ef66e
yeswanth <yswnth.2010@gmail.com>
2024-09-01 15:41:09 +0530

    shell practice

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -14,8 +14,8 @@ N="\e[0m"       # normal or no color
 
 while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
 do
-    USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
         echo "$PARTITION is $B more than ${DISK_THRESHOLD} $N, current value: $R ${USAGE} $N. Please check"
